0aa50aa8f656bf9fd74d3873e7fb8071a5587d4c
ganghe74 <ganghe74@kookmin.ac.kr>
2019-04-23 23:55:29 +0900

    pointer is crazy....🤦‍♀️

--- WTF.c
+++ WTF.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 void main() {
     int i;
-    int arr[5];
+    int arr[5] = {1,2,3,4,5};
 
     i = 1234;
     arr[5] = 5678;
-    printf("i=%d\n", i);
+    printf("i = %d\n", i);
+
+    printf("arr[1] = %d\n", arr[1]);
+    printf("1[arr] = %d\n", 1[arr]);
+    printf("*(arr+1) = %d\n", *(arr+1));
 }
